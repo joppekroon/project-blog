25dc203cb8094c04726fbca20d233f2617c66aa2
Joppe <joppekroon@gmail.com>
2026-08-28 11:44:32 +0200

    Exercise 6: Circular colors widget

--- src/components/CircularColorsDemo/CircularColorsDemo.js
+++ src/components/CircularColorsDemo/CircularColorsDemo.js
@@ -1,15 +1,13 @@
+'use client';
 import React from 'react';
 import clsx from 'clsx';
-import {
-	Play,
-	Pause,
-	RotateCcw,
-} from 'react-feather';
+import { Play, Pause, RotateCcw } from 'react-feather';
 
 import Card from '@/components/Card';
 import VisuallyHidden from '@/components/VisuallyHidden';
 
 import styles from './CircularColorsDemo.module.css';
+import { motion, LayoutGroup } from 'motion/react';
 
 const COLORS = [
 	{ label: 'red', value: 'hsl(348deg 100% 60%)' },
@@ -18,68 +16,87 @@ const COLORS = [
 ];
 
 function CircularColorsDemo() {
-	// TODO: This value should increase by 1 every second:
-	const timeElapsed = 0;
+	const id = React.useId();
+	const [timerRunning, setTimerRunning] = React.useState(false);
+	const [timeElapsed, setTimeElapsed] = React.useState(0);
 
-	// TODO: This value should cycle through the colors in the
-	// COLORS array:
-	const selectedColor = COLORS[0];
+	const selectedColor = COLORS[timeElapsed % COLORS.length];
+
+	React.useEffect(() => {
+		if (timerRunning) {
+			const intervalId = setInterval(() => {
+				setTimeElapsed((value) => value + 1);
+			}, 1000);
+
+			return () => {
+				console.log('clearing');
+				clearInterval(intervalId);
+			};
+		}
+	}, [timerRunning]);
+
+	function handleToggleTimer() {
+		setTimerRunning(!timerRunning);
+	}
+
+	function handleResetTimer() {
+		setTimerRunning(false);
+		setTimeElapsed(0);
+	}
 
 	return (
-		<Card as="section" className={styles.wrapper}>
-			<ul className={styles.colorsWrapper}>
-				{COLORS.map((color, index) => {
-					const isSelected =
-						color.value === selectedColor.value;
+		<LayoutGroup>
+			<Card as="section" className={styles.wrapper}>
+				<ul className={styles.colorsWrapper}>
+					{COLORS.map((color, index) => {
+						const isSelected = color.value === selectedColor.value;
 
-					return (
-						<li
-							className={styles.color}
-							key={index}
-						>
-							{isSelected && (
-								<div
-									className={
-										styles.selectedColorOutline
-									}
-								/>
-							)}
-							<div
-								className={clsx(
-									styles.colorBox,
-									isSelected &&
-										styles.selectedColorBox
+						return (
+							<li className={styles.color} key={index}>
+								{isSelected && (
+									<motion.div
+										layoutId={`${id}-outline`}
+										className={styles.selectedColorOutline}
+									/>
 								)}
-								style={{
-									backgroundColor: color.value,
-								}}
-							>
-								<VisuallyHidden>
-									{color.label}
-								</VisuallyHidden>
-							</div>
-						</li>
-					);
-				})}
-			</ul>
+								<div
+									className={clsx(
+										styles.colorBox,
+										isSelected && styles.selectedColorBox,
+									)}
+									style={{
+										backgroundColor: color.value,
+									}}
+								>
+									<VisuallyHidden>{color.label}</VisuallyHidden>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
 
-			<div className={styles.timeWrapper}>
-				<dl className={styles.timeDisplay}>
-					<dt>Time Elapsed</dt>
-					<dd>{timeElapsed}</dd>
-				</dl>
-				<div className={styles.actions}>
-					<button>
-						<Play />
-						<VisuallyHidden>Play</VisuallyHidden>
-					</button>
-					<button>
-						<RotateCcw />
-						<VisuallyHidden>Reset</VisuallyHidden>
-					</button>
+				<div className={styles.timeWrapper}>
+					<dl className={styles.timeDisplay}>
+						<dt>Time Elapsed</dt>
+						<dd>{timeElapsed}</dd>
+					</dl>
+					<div className={styles.actions}>
+						<button
+							type="button"
+							aria-pressed={timerRunning}
+							onClick={handleToggleTimer}
+						>
+							{timerRunning ? <Pause /> : <Play />}
+							<VisuallyHidden>Play</VisuallyHidden>
+						</button>
+						<button type="button" onClick={handleResetTimer}>
+							<RotateCcw />
+							<VisuallyHidden>Reset</VisuallyHidden>
+						</button>
+					</div>
 				</div>
-			</div>
-		</Card>
+			</Card>
+		</LayoutGroup>
 	);
 }
 
